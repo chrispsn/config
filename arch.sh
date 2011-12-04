6644e8b1231a71e1ee113d6dbdc0e1a9c7f86f34
Chris Pearson <chrispsn@gmail.com>
2011-12-04 20:13:03 +1100

    Updated arch.sh

--- arch.sh
+++ arch.sh
@@ -6,7 +6,9 @@ sudo pacman -S rxvt-unicode
 sudo pacman -S zsh
 
 # Other installs
-
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+chsh -s /bin/zsh
 
 # Symlinks
-
+ln -s ~/config/.zshrc ~/.zshrc
+ln -s ~/config/.xinitrc ~/xinitrc
