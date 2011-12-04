2b42b3922f5192fc9d406433d018b7d57f4205cf
Chris Pearson <chrispsn@gmail.com>
2011-12-04 20:33:05 +1100

    Updated install scripts...

--- arch.sh
+++ arch.sh
@@ -1,4 +1,4 @@
-# Packages
+echo "Installing packages..."
 sudo pacman -S gvim
 sudo pacman -S xmonad
 sudo pacman -S xmonad-contrib
@@ -6,11 +6,22 @@ sudo pacman -S dzen
 sudo pacman -S rxvt-unicode
 sudo pacman -S zsh
 
-# Other installs
+echo "Setting up oh-my-zsh, a zsh prettifier..."
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 chsh -s /bin/zsh
 
-# Symlinks
+echo "Set up paktahn, an AUR helper. Instructions are in arch.sh; can't run it automatically yet because the version number might change for pacman -U purposes, so do it manually."
+# echo "Setting up paktahn, an AUR helper..."
+# cd ~
+# mkdir aur
+# cd aur
+# wget aur.archlinux.org/packages/pa/paktahn/paktahn.tar.gz
+# tar -xzvf paktahn.tar.gz
+# cd paktahn
+# makepkg -s
+# sudo pacman -U <paktahn version>.tar.xz
+
+echo "Setting up symlinks..."
 ln -s ~/config/.zshrc ~/.zshrc
 ln -s ~/config/.xinitrc ~/xinitrc
 ln -s ~/config/.Xdefaults ~/.Xdefaults
